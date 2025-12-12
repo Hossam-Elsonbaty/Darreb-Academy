9266a1cd868c6fe96992d774c532e38c21781856
Mostafa Elsonbaty <armaggg3@gmail.com>
2025-12-12 21:17:59 +0200

    implemnt footer

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../common/navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../common/footer/Footer";
 
 const RootLayout = () => {
   return (
     <main className=" bg-lightGreen">
       <Navbar />
       <Outlet />
+      <Footer/>
     </main>
   );
 };
