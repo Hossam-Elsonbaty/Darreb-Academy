4bf135a4ab3b274ca5e9f72f76b0dfee86e7a1bd
Mostafa Elsonbaty <armaggg3@gmail.com>
2025-12-25 13:03:50 +0200

    Update CourseWatch.jsx

--- src/pages/watchCourse/CourseWatch.jsx
+++ src/pages/watchCourse/CourseWatch.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import api from "../../api/axios";
 import { 
   Share2, 
   Users, 
@@ -28,13 +28,8 @@ const CourseWatch = () => {
     const fetchCourseDetails = async () => {
       try {
         setLoading(true);
-        const response = await axios.get(
-          `http://localhost:5000/api/users/me/courses/${id}`,
-          {
-            headers: {
-              Authorization: `Bearer ${apiKey}`,
-            },
-          }
+        const response = await api.get(
+          `/users/me/courses/${id}`
         );
         setCourseData(response.data.data);
         
